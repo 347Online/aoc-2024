1cb6d54d675980396e7abf353753f80c13696200
347Online | Katie Janzen <katiejanzen@347online.me>
2024-12-03 10:32:10 -0600

    Day1A success

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs::read_to_string};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_to_string("./input/day1a.txt")?;
+
+    let mut a = vec![];
+    let mut b = vec![];
+
+    for (x, y) in input.lines().map(|x| {
+        let (a, b) = x.split_once("   ").unwrap();
+        (a, b)
+    }) {
+        a.push(x);
+        b.push(y);
+    }
+
+    a.sort();
+    b.sort();
+
+    let result = a.iter().zip(b.iter()).fold(0, |acc, (x, y)| {
+        let x: i32 = x.parse().unwrap();
+        let y: i32 = y.parse().unwrap();
+        acc + (x - y).abs()
+    });
+
+    println!("{result}");
+
+    Ok(())
 }
